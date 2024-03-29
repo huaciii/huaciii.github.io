93b6ff495a0548fcaa871b8b5e82eb755d771770
huaciii <123053250+huaciii@users.noreply.github.com>
2024-03-29 12:29:03 +0900

    Add files via upload

---
+++ portfilo/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/homepage.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+      'sign': "url('./img/sign.png')",
+  },
+  plugins: [],
+}}}
